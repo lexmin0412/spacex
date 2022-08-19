e5272054b7b0ff49a8ee6b5254741c8751e6b09a
lexmin0412 <zhangle_dev@outlook.com>
2022-08-20 00:56:16 +0800

    refactor: react-router basename 适配

--- src/routes/entry.tsx
+++ src/routes/entry.tsx
@@ -8,7 +8,7 @@ import Layout from './../components/layouts/layout'
 
 export default function RouterEntry() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename='/spacex'>
 			<Layout>
 				<Routes>
 					{
